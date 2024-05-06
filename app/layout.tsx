33f0573d2b6bff1e27d72311845d68ccf663ee7d
Congglee <congldqn888@gmail.com>
2024-05-06 09:22:19 +0700

    Page layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Separator } from "@/components/ui/separator";
 import NavBar from "@/components/NavBar";
+import { ThemeProvider } from "@/providers/ThemeProvider";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,13 +21,21 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>
-          <div className="flex flex-col items-center min-h-screen w-full dark:bg-black">
-            <NavBar />
-            <Separator />
-            {children}
-          </div>
+      <html
+        lang="en"
+        className={cn(inter.className, "dark")}
+        style={{ colorScheme: "dark" }}
+      >
+        <body>
+          <ThemeProvider>
+            <div className="flex flex-col items-center min-h-screen w-full dark:bg-black">
+              <NavBar />
+              <Separator />
+              <main className="flex flex-grow w-full justify-center items-center dark:bg-neutral-950">
+                {children}
+              </main>
+            </div>
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -1,5 +1,35 @@
-import React from "react";
+"use client";
+
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { DesktopIcon, MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { useTheme } from "next-themes";
+import React, { useEffect, useState } from "react";
 
 export default function ThemeSwitcher() {
-  return <div>ThemeSwitcher</div>;
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  // useEffect only runs on the client, so now can safely show the UI
+  // avoid hydration errors
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <Tabs defaultValue={theme}>
+      <TabsList className="border dark:border-neutral-800 dark:bg-[#303030]">
+        <TabsTrigger value="light" onClick={(e) => setTheme("light")}>
+          <SunIcon className="h-[1.2rem] w-[1.2rem]" />
+        </TabsTrigger>
+        <TabsTrigger value="dark" onClick={(e) => setTheme("dark")}>
+          <MoonIcon className="h-[1.2rem] w-[1.2rem] rotate-90 transition-all dark:rotate-0" />
+        </TabsTrigger>
+        <TabsTrigger value="system" onClick={(e) => setTheme("system")}>
+          <DesktopIcon className="h-[1.2rem] w-[1.2rem]" />
+        </TabsTrigger>
+      </TabsList>
+    </Tabs>
+  );
 }
